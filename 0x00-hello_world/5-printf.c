65dded22b086452e642780bda788650c4187d1f4
ummarere <ubdodo@gmail.com>
2023-02-19 07:03:42 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'usng printf function'
+ *
+ * Return: Always 0 (Succeess)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
