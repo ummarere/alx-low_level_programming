9438ee22bd1eef1a56dfac3e98988543bf00b9c8
ummarere <ubdodo@gmail.com>
2023-02-19 05:45:55 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/*main - entry point
+/**
+ * main - Entry point
  *
- *retun: always 0
+ * Retun: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
