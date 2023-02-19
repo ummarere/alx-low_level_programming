893ffe5f782215e697d2380b029a9a87497080a4
ummarere <ubdodo@gmail.com>
2023-02-19 05:35:17 -0800

    Hello pus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+/*main - entry point
+ *retun: always 0
+*/
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
