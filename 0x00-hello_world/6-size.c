488e8826af9cb7c9af6f78dc78ed174e3709acd1
ummarere <ubdodo@gmail.com>
2023-02-19 23:45:29 -0800

    size og variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: 'Size is not grandeur and territory does not make a ntn'
+ * Description: 'sizes of variables'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char u;
-	int v;
+	char v;
+	int w;
 	long int x;
-       	long long y;
+	long long y;
 	float z;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(u));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(v));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(W));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(y));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 	return (0);
 }
