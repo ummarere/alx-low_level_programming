4d456c917f6cb7f629bd8fda04763119f92d8f96
ummarere <ubdodo@gmail.com>
2023-02-19 23:58:12 -0800

    soze of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,12 @@ int main(void)
 	char v;
 	int w;
 	long int x;
-	long long y;
+	long long int  y;
 	float z;
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(W));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 	return (0);
 }
