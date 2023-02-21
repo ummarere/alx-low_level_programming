f10b826e8288005a1bfa1b2ac0694a350a6588ae
ummarere <ubdodo@gmail.com>
2023-02-21 14:01:18 -0800

    nested while loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in small letters
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
