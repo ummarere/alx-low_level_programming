d9a9ec1e0b5b1a11ba19a6d0a96fba9303b9f31e
ummarere <ubdodo@gmail.com>
2023-02-28 13:10:37 +0100

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,6 +4,7 @@
  * print_rev - this function prints strings in reverse
  *
  * @s: the string to be printed in reverse order
+ *
  */
 
 void print_rev(char *s)
@@ -11,7 +12,7 @@ void print_rev(char *s)
 	int i, j;
 	char temp;
 
-	for (i = 0, j = _strlen(s) - 1; i < j; i++, j--)
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
