8282551b5701cd32667ee9013da8b1672b0c1c86
ummarere <ubdodo@gmail.com>
2023-04-04 15:51:41 -0700

    Find the loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,29 @@
 #include "lists.h"
+
 /**
- * print_listint_safe - thsi function print a list in safe mode
- * @head: the head of list
- * Description: this function print a string in a safe mode
- * section header: the header of this function is lists.h)*
- * Return: the size of the list
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ *
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int difference, i, number;
-	void *address;
+	size_t num = 0;
+	long int diff;
 
-	i = 0;
-
-	while (head != NULL)
+	while (head)
 	{
-		difference = head - head->next;
-		i++;
-		number = head->n;
-		printf("[%p] %i\n", (void *)head, number);
-		if (difference > 0)
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
 			head = head->next;
 		else
 		{
-			address = head->next;
-			number = head->next->n;
-			printf("-> [%p] %i\n", address, number);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
-
 	}
-	return (i);
+
+	return (num);
 }
